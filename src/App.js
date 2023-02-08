816d3709434d9be608487fa4ba72cc8c2e1ec0b7
Gualberto <gumodi10@gmail.com>
2023-02-08 16:34:44 -0600

    Add markdown previewer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { marked } from "marked";
+import "./App.css";
+
+/* a heading element (H1 size), a sub heading element (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.*/
 
 function App() {
+  const [text, setText] = useState(`
+  # H1
+  ## H2
+  [link](https://www.freecodecamp.org/)
+  \`inline code\`
+  \`\`\`
+  code block
+  \`\`\`
+  * list item
+  > blockquote
+  ![image](https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/FreeCodeCamp_logo.png/800px-FreeCodeCamp_logo.png)
+  **bolded text**
+  `);
+
+  marked.setOptions({
+    breaks: true
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        id="editor"
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+        value={text}
+      ></textarea>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{
+          __html: marked(text),
+        }}
+      ></div>
     </div>
   );
 }
